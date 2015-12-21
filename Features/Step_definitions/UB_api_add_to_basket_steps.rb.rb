fca93e4674499fc64dee250127c0c2daf68feba9
keith <keith.ford1980@googlemail.com>
2015-12-21 18:20:04 +0000

    building headers

--- Features/Step_definitions/UB_api_add_to_basket_steps.rb.rb
+++ Features/Step_definitions/UB_api_add_to_basket_steps.rb.rb
@@ -9,11 +9,25 @@ end
 When(/^I request through the api the item is added to the basket$/) do
   #sleep 100000
   puts @user_token
-  puts JSON.generate(@user_token)
-  puts HTTParty.get('http://api.ub.io/user/me',  :headers => {'access_token' => @user_token})
+  access_json = JSON.generate(@user_token)
+  access_token_parsed = JSON.parse(access_json, :symbolize_names => true)
+  puts access_token_parsed.fetch(:userId)
+  puts HTTParty.get('http://api.ub.io/user/me',  :headers => {'userId' => access_token_parsed.fetch(:userId).to_s,
+                                                              'appId' => access_token_parsed.fetch(:appId).to_s,
+                                                              'countryId' => access_token_parsed.fetch(:countryId).to_s,
+                                                              'token' => access_token_parsed.fetch(:token).to_s,
+                                                              'scope' => access_token_parsed.fetch(:scope).to_s,
+                                                              'expiryDate' => access_token_parsed.fetch(:expiryDate).to_s,
+                                                              'id' => access_token_parsed.fetch(:id).to_s})
   api_auth_host = API_BASE + 'basket/add/:' + @prod_id
   puts api_auth_host
-  response = HTTParty.post(api_auth_host, :header =>  @user_token)
+  response = HTTParty.post(api_auth_host, :headers => {'userId' => access_token_parsed.fetch(:userId).to_s,
+                                                       'appId' => access_token_parsed.fetch(:appId).to_s,
+                                                       'countryId' => access_token_parsed.fetch(:countryId).to_s,
+                                                       'token' => access_token_parsed.fetch(:token).to_s,
+                                                       'scope' => access_token_parsed.fetch(:scope).to_s,
+                                                       'expiryDate' => access_token_parsed.fetch(:expiryDate).to_s,
+                                                       'id' => access_token_parsed.fetch(:id).to_s})
   @response = response
-  puts response
+  puts response.body
 end
