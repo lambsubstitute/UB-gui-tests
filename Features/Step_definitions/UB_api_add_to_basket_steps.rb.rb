6dea33368508cea5beb88f4eff5c88028df62e23
keith <keith.ford1980@googlemail.com>
2016-01-06 17:56:59 +0000

    removing broken farfetch

--- Features/Step_definitions/UB_api_add_to_basket_steps.rb.rb
+++ Features/Step_definitions/UB_api_add_to_basket_steps.rb.rb
@@ -20,6 +20,8 @@ When(/^I request through the api the item is added to the basket$/) do
                            :body => {:productId => @prod_id})
   @response = response
   puts response.body
+  body = response.body
+  assert (body.include? 'Product is out of stock') == false, 'product was not in stock, so failing test, check you can still crawl products from this shop by checking the product'
 end
 
 Then(/^the iem requested should be in the new basket$/) do
